6775fdd10baf525eede7050dab05bc3a9b115a58
Raúl Montes <raulmt@gmail.com>
2015-09-02 23:56:46 -0300

    Agrega validaciones a User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ActiveRecord::Base
+  validates :first_name, presence: true, length: {minimum: 2}
+  validates :last_name, presence: true
+  validates :password, presence: true
+  # TODO: validación de formato de e-mail bien primitiva por el momento...
+  validates :email, presence: true, uniqueness: true, format: /\A[a-z0-9\._-]+@[a-z0-9\._-]+\.[a-z]{1,5}\z/
+
 end
