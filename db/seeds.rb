17b0e8af97eca7eb615bf96dba8bd83ca9083fb2
Raúl Montes <raulmt@gmail.com>
2015-09-09 23:16:26 -0300

    Agrega seed para crear ngos e iniciativas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+ngo_names = ['CEMA Chile', 'Fundación Chol-Chol', 'Consejo de Defensa del Niño', 'Corporación para la Nutrición Infantil', 'Cruz Roja Chilena', 'Desafío Levantemos Chile', 'Educación 2020', 'Enseña Chile', 'Fundación Futuro', 'Fundación Iguales', 'Fundación para la Superación de la Pobreza', 'Neutralidad Si', 'ONG Derechos Digitales', 'Servicio País', 'Wikipedia:Wikimedia Chile', 'Worldwide Project']
+
+ngos = ngo_names.map do |name|
+  email = Faker::Internet.email(name)
+  url = email.gsub(/.+@/, 'www.')
+  Ngo.create!(
+    name: name,
+    description: Faker::Lorem.paragraph(2),
+    email: email,
+    webpage: url
+  )
+end
+
+ngos.each do |ngo|
+  10.times do
+    ngo.initiatives << Initiative.new(
+      title: Faker::Lorem.words(3).map(&:capitalize).join(' '),
+      description: Faker::Lorem.paragraphs(3).join("\n"),
+      active: true,
+      hashtag: Faker::Lorem.words(1)[0]
+    )
+  end
+end
