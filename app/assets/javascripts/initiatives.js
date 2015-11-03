fb136f11be235ce99072e1d8c00dc7e383c5dc24
Raúl Montes <raulmt@gmail.com>
2015-11-03 20:32:11 -0300

    Envío de formulario para firmar mediante Ajax

--- app/assets/javascripts/initiatives.js
+++ app/assets/javascripts/initiatives.js
@@ -1,2 +1,8 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+$(document).on('page:change', function() {
+  // Escuchamos el evento ajax:success, que emitirán los requests Ajax
+  // generados por Rails al recibir una respuesta exitosa
+  $('#new_initiative_sign').on('ajax:success', function(event, data, status, xhr) {
+    // reemplazamos el contenido html de este elemento por el que nos llegó en la respuesta al request Ajax
+    $('#signs-container').html(data);
+  });
+});
--- app/controllers/initiative_signs_controller.rb
+++ app/controllers/initiative_signs_controller.rb
@@ -1,5 +1,27 @@
 class InitiativeSignsController < ApplicationController
   def create
+    @initiative = Initiative.find(params[:initiative_id])
+    @initiative_sign = InitiativeSign.create(initiative_sign_params.merge(user_id: current_user.id, initiative_id: params[:initiative_id]))
+
+    # podemos responder de manera diferente ya sea
+    # examinando si el request es o no Ajax (xhr)
+    if request.xhr?
+      @signs_count = @initiative.signs.count
+      @user_sign = @initiative.signs.of_user(current_user.id) if current_user
+      render partial: 'initiatives/signs'
+    else
+      redirect_to ngo_initiative_path(initiative.ngo, initiative)
+    end
+
+    # o podríamos hacerlo simplemente por el content type pedido
+    # en el request:
+    # respond_to do |format|
+    #   format.html {}
+    #   # éste es tipo default pedido en requests Ajax generados por Rails.
+    #   # y es procesado automáticamente por el generador del request (se ejecutará código JS enviado)
+    #   format.js { render text: 'alert("Gracias por firmar");' }
+    # end
+  end
     initiative = Initiative.find(params[:initiative_id])
     initiative_sign = InitiativeSign.create(initiative_sign_params.merge(user_id: current_user.id, initiative_id: params[:initiative_id]))
     redirect_to ngo_initiative_path(initiative.ngo, initiative)
